81d545a2b40b4b8fa3281055ecffc0164d8affaa
Marck1js <marckk63prog@gmail.com>
2024-12-04 14:17:48 -0300

    nineth commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,16 +4,30 @@ export default {
   theme: {
     extend: {
       colors: {
-        'main':"#fff",
+        'main':"#E4EBFA",
         'dark-main':"#0F172A",
         'primary':'#000',
         'dark-primary':'#e2e8f0',
-        'border':'#edeef3',
+        'border':'#bbb',
         'dark-border':'#cbd5e11a',
         'option':'#334155',
         'dark-option':'#94a3b8',
-        'active':"#3d50f5",
-        "dark-active":"#2e2b5f"
+        "active":"#3d50f5",
+        'dark-active':"#1c2995",
+
+        // # Colores Kanban
+        'main-purple':'#635fc7',
+        'main-purple-hover':'#a8a4ff',
+        'black':'#000112',
+        'very-dark-grey':'#20212c',
+        'dark-grey':'#2b2c37',
+        'lines-dark':'#3e3f4e',
+        'medium-grey':'#828fa3',
+        'lines-light':'#e4ebfa',
+        'light-grey':'#f4f7fd',
+        'light-red':'#ea5555',
+        'light-red-hover':'#ff9898',
+        
       },
       height: {
         fix: "calc(100vh - 64px)",
