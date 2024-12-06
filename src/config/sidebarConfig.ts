1f1788aeb823821f073f0fe0437ba7d5091a39a6
Marco Juarez <marco.juarez@bvstv.com>
2024-12-06 17:13:26 -0300

    tenth

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -1,84 +1,162 @@
 // sidebarConfig.js
-export const sidebarConfig = [
+
+interface Topic {
+  name: string;
+  path?: string;
+  route: {
+    concept: {
+      label?: string;
+      path?: string;
+    };
+  }[];
+}
+
+export const sidebarConfigTopic: Topic[] = [
   {
-    name: 'Fundamentals of Networking',
-    tags: [
-      'Conceptos básicos de redes',
-      'Modelos OSI y TCP/IP',
-      'Tipos de redes (LAN, WAN, etc.)',
-      'Dirección IP y subredes',
-    ],
-    paths: [
-      '/fundamentos/conceptos-basicos',
-      '/fundamentos/modelos-osi-y-tcpip',
-      '/fundamentos/tipos-de-redes',
-      '/fundamentos/direccion-ip-subredes'
-    ]
-  },
-  {
-    name: 'Network Access',
-    tags: [
-      'Configuración de switches',
-      'VLANs y su funcionamiento',
-      'Troncalización de VLANs',
-      'Protocolos de acceso (STP, RSTP)',
-    ],
-    paths: [
-      '/network-access/configuracion-de-switches',
-      '/network-access/vlan-funcionamiento',
-      '/network-access/trunking',
-      '/network-access/protocolos-de-acceso'
-    ]
-  },
-  {
-    name: 'IP Connectivity',
-    tags: [
-      'Enrutamiento estático y dinámico',
-      'Protocolos de enrutamiento (RIP, OSPF, EIGRP)',
-      'Concepto de rutas y tablas de enrutamiento',
-      'Direcciones IP públicas y privadas',
-    ],
-    paths: [
-      '/ip-connectivity/enrutamiento-estatico-dinamico',
-      '/ip-connectivity/protocolos-de-enrutamiento',
-      '/ip-connectivity/concepto-de-rutas-y-tablas-de-enrutamiento',
-      '/ip-connectivity/direcciones-ip-publicas-y-privadas'
-    ]
-  },
-  {
-    name: 'IP Services',
-    tags: [
-      'DHCP y su configuración',
-      'NAT (Traducción de direcciones de red)',
-      'DNS y su funcionamiento',
-      'QoS (Calidad de Servicio)',
+    name: "Fundamentals of Networking",
+    path: "/fundamentos",
+    route: [
+      {
+        concept: {
+          label: "Conceptos básicos de redes",
+          path: "/fundamentos/conceptos-basicos",
+        },
+      },
+      {
+        concept: {
+          label: "Modelos OSI y TCP/IP",
+          path: "/fundamentos/modelos-osi-y-tcpip",
+        },
+      },
+      {
+        concept: {
+          label: "Tipos de redes (LAN, WAN, etc.)",
+          path: "/fundamentos/tipos-de-redes",
+        },
+      },
+      {
+        concept: {
+          label: "Dirección IP y subredes",
+          path: "/fundamentos/direccion-ip-subredes",
+        },
+      },
     ],
   },
   {
-    name: 'Security Fundamentals',
-    tags: [
-      'Seguridad en redes',
-      'Lista de control de acceso (ACL)',
-      'VPNs y su configuración',
-      'Seguridad en dispositivos de red',
+    name: "Network Access",
+    path: "",
+    route: [
+      {
+        concept: {
+          label: "Configuración de switches",
+          path: "/network-access/configuracion-de-switches",
+        },
+      },
+      {
+        concept: {
+          label: "VLANs y su funcionamiento",
+          path: "/network-access/vlan-funcionamiento",
+        },
+      },
+      {
+        concept: {
+          label: "Troncalización de VLANs",
+          path: "/network-access/trunking",
+        },
+      },
+      {
+        concept: {
+          label: "Protocolos de acceso (STP, RSTP)",
+          path: "/network-access/protocolos-de-acceso",
+        },
+      },
     ],
   },
   {
-    name: 'Automation and Programmability',
-    tags: [
-      'Introducción a la automatización de redes',
-      'Herramientas de programación (Python, Ansible)',
-      'APIs y su uso en redes',
-      'Modelos de red definidas por software (SDN)',
+    name: "IP Connectivity",
+    path: "",
+    route: [
+      {
+        concept: {
+          label: "Enrutamiento estático y dinámico",
+          path: "/ip-connectivity/enrutamiento-estatico-dinamico",
+        },
+      },
+      {
+        concept: {
+          label: "Protocolos de enrutamiento (RIP, OSPF, EIGRP)",
+          path: "/ip-connectivity/protocolos-de-enrutamiento",
+        },
+      },
+      {
+        concept: {
+          label: "Concepto de rutas y tablas de enrutamiento",
+          path: "/ip-connectivity/concepto-de-rutas-y-tablas-de-enrutamiento",
+        },
+      },
+      {
+        concept: {
+          label: "Direcciones IP públicas y privadas",
+          path: "/ip-connectivity/direcciones-ip-publicas-y-privadas",
+        },
+      },
     ],
   },
   {
-    name: 'Examen y Preparación',
-    tags: [
-      'Estrategias de estudio',
-      'Simuladores de examen',
-      'Recursos y materiales recomendados',
-      'Consejos para el día del examen',
+    name: "IP Services",
+    path: "",
+    route: [
+      { concept: { label: "DHCP y su configuración", path: "" } },
+      {
+        concept: { label: "NAT (Traducción de direcciones de red)", path: "" },
+      },
+      { concept: { label: "DNS y su funcionamiento", path: "" } },
+      { concept: { label: "QoS (Calidad de Servicio)", path: "" } },
+    ],
+  },
+  {
+    name: "Security Fundamentals",
+    path: "",
+    route: [
+      { concept: { label: "Seguridad en redes", path: "" } },
+      { concept: { label: "Lista de control de acceso (ACL)", path: "" } },
+      { concept: { label: "VPNs y su configuración", path: "" } },
+      { concept: { label: "Seguridad en dispositivos de red", path: "" } },
+    ],
+  },
+  {
+    name: "Automation and Programmability",
+    path: "",
+    route: [
+      {
+        concept: {
+          label: "Introducción a la automatización de redes",
+          path: "",
+        },
+      },
+      {
+        concept: {
+          label: "Herramientas de programación (Python, Ansible)",
+          path: "",
+        },
+      },
+      { concept: { label: "APIs y su uso en redes", path: "" } },
+      {
+        concept: {
+          label: "Modelos de red definidas por software (SDN)",
+          path: "",
+        },
+      },
+    ],
+  },
+  {
+    name: "Examen y Preparación",
+    path: "",
+    route: [
+      { concept: { label: "Estrategias de estudio", path: "" } },
+      { concept: { label: "Simuladores de examen", path: "" } },
+      { concept: { label: "Recursos y materiales recomendados", path: "" } },
+      { concept: { label: "Consejos para el día del examen", path: "" } },
     ],
   },
 ];
